b40a12a266a036f09884d672c2d39bf6be2b2b24
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2023-05-27 20:19:04 +0700

    Add files via upload

---
+++ links.js
@@ -0,0 +1,32 @@
+export const links = [
+    {
+      name: "Twitter",
+      link: "https://twitter.com/ringgarevanka/",
+      icon: "fab fa-twitter",
+    },
+    {
+      name: "Linkedin",
+      link: "https://www.linkedin.com/ringgarevanka/",
+      icon: "fab fa-linkedin",
+    },
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com/ringgarevanka/",
+      icon: "fab fa-instagram",
+    },
+    {
+      name: "E-mail",
+      link: "mailto:ringgarevanka+public@gmail.com",
+      icon: "fa fa-envelope",
+    },
+    {
+      name: "Github",
+      link: "https://github.com/ringgarevanka",
+      icon: "fab fa-github",
+    },
+    {
+      name: "Medium",
+      link: "https://medium.com/ringgarevanka",
+      icon: "fab fa-medium",
+    },
+  ];
