cd227abbe3994b326e544cc9bf23a04ad61f3b84
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2023-06-09 15:56:18 +0700

    Update & Upgrade

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.onload = () => {
          .then(function () {
             console.log('Caches Service Registered (DONE)');
             console.log('Page Loaded');
-            alert('WELCOME, THIS PAGE IS UNDER DEVELOPMENT MAY SOME FEATURES DON T WORK YET');
+            alert('');
          });
    }
 }
