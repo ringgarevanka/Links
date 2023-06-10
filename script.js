17a815ec1a868e7024d486944b13cb719a133963
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2023-06-10 14:13:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-/* cached service */
 window.onload = () => {
    'use strict';
 
    if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-         .register('./minicache.js')
+         .register('./serviceWorker.js')
          .then(function () {
             console.log('Caches Service Registered (DONE)');
             console.log('Page Loaded');
-            alert('Welcome, This page is not fully finished, some features may not be accessible yet.');
          });
    }
 }
