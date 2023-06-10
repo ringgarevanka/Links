87ad25ba7364b984d1c714ce01ad1cbf40b6bba2
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2023-06-10 21:09:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ window.onload = () => {
          });
    }
 }
+
+window.onload = function() {
+    alert("Announcement: This page is still under development, may have errors or bugs.
+
+Last Updated: 10.6.2023
+
+@ringgarevanka");
+}
